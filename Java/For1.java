097a1dbc6f6603aedaff36738fe2ca42a7468460
utkarsh Tiwari <91utkarsh@gmail.com>
2025-11-28 17:25:48 +0530

    Second commit

---
+++ Java/For1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class For1 {
+    public static void main (String args[]){
+        Scanner s1=new Scanner(System.in);
+        System.out.println("enter the number of time you want to print:");
+        int n=s1.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("utkarsh");
+        }   System.out.println("tiwari");
+    }
+}
