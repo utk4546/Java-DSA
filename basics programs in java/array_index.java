4c4c24cb3e1c214f9fa345453cd9ca117dc08c58
utk4546 <91utkarsh@gmail.com>
2025-12-16 20:23:08 +0530

    indexes

---
+++ basics programs in java/array_index.java
@@ -0,0 +1,10 @@
+class arra_index{
+    public static void main(String[] args) {
+        int arr[]={20,59,39,43,50};
+        for(int i=0;i<5;i++){
+            System.out.println("index no is "+i);
+            System.out.println("this the data on above index = "+arr[i]);
+        }
+
+    }
+}
